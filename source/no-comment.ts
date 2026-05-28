1d47e5a74ed886e43dd62c13ee5a2a7e254a7533
Stephan Schreiber <septh@users.noreply.github.com>
2026-05-28 16:27:40 +0200

    refactor(no-comment): rewrite the comments raking method using a safer algorithm

--- source/no-comment.ts
+++ source/no-comment.ts
@@ -1,10 +1,12 @@
 import MagicString from 'magic-string'
+import { walk } from 'zimmerframe'
+import type { Node as AstNode } from 'estree'
 import type { Plugin } from 'rollup'
-import { CharCode, whitespace, lineTerminators } from './lib/charcode.js'
+import { lineTerminators, space } from './lib/charcode.js'
 
 export interface NoCommentOptions {
     /** Keep license (`/*!`) comments? */
-    keepLicences?: boolean | undefined
+    keepLicenses?: boolean | undefined
     /** Keep JSDoc/TSDoc (`/**`) comments? */
     keepDocs?: boolean | undefined
     /** Keep annotations (`__PURE__`, `__NO_SIDE_EFFECTS__`) comments? */
@@ -12,30 +14,45 @@ export interface NoCommentOptions {
 }
 
 /** Removes residual comments in the bundle. */
-export default function noComment({ keepLicences = false, keepDocs = false, keepAnnotations = false }: NoCommentOptions = {}): Plugin {
+export default function noComment({ keepLicenses = false, keepDocs = false, keepAnnotations = false }: NoCommentOptions = {}): Plugin {
 
     const licenseStartRx  = /^\/\*\![ \r\n\u2028\u2029]/    // /*!<space or line terminator>
     const docStartRx      = /^\/\*\*[ \r\n\u2028\u2029]/    // /**<space or line terminator>
     const docLicenseTagRx = /\s@license\b/
     const annotationRx    = /[@#]__(?:PURE|NO_SIDE_EFFECTS)__/
 
-    const removeLicenses = !keepLicences
-    const removeDocs = !keepDocs
-    const removeAnnotations = !keepAnnotations
-
     return {
         name: 'no-comment',
 
         renderChunk(code) {
             const ms = new MagicString(code)
-            remove_comments(ms, comment => Boolean(
-                licenseStartRx.test(comment) ? removeLicenses
-                : docStartRx.test(comment) ? (
-                    docLicenseTagRx.test(comment) ? removeLicenses : removeDocs
-                )
-                : annotationRx.test(comment) ? removeAnnotations
-                : true // Meaningless comments are always removed
-            ))
+
+            const shouldRemove = (comment: string) => !(
+                licenseStartRx.test(comment) ? keepLicenses
+                    : docStartRx.test(comment) ?
+                        docLicenseTagRx.test(comment) ? keepLicenses : keepDocs
+                    : annotationRx.test(comment) ? keepAnnotations
+                    : false
+            )
+
+            let prev = { start: NaN, end: NaN } as AstNode
+            walk(this.parse(code) as AstNode, {}, {
+                _(node, context) {
+                    if (node.start >= prev.start && node.end <= prev.end) {
+                        // `node` is the first child of `prev`
+                        if ((node.start - prev.start) > 1) {
+                            // And there is text before it.
+                            scanTextBetweenNodes(ms, prev.start, node.start, shouldRemove)
+                        }
+                    }
+                    else if ((node.start - prev.end) > 1) {
+                        // `node` immediately follows `prev` and there is text between them.
+                        scanTextBetweenNodes(ms, prev.end, node.start, shouldRemove)
+                    }
+                    prev = node
+                    context.next()
+                }
+            })
 
             return ms.hasChanged()
                 ? { code: ms.toString(), map: ms.generateMap() }
@@ -43,140 +60,53 @@ export default function noComment({ keepLicences = false, keepDocs = false, keep
         }
     }
 
-    function remove_comments(ms: MagicString, shouldRemove: (comment: string) => boolean) {
-        const { original: code } = ms,
-              end = code.length
-        return scan(0, false)
+    function scanTextBetweenNodes(ms: MagicString, from: number, to: number, shouldRemoveComment: (comment: string) => boolean): void {
+        const re = /(?<line>[/][/][^\n]*)|(?<block>[/][*].*?[*][/])/gsd
 
-        function scan(pos: number, inTemplate: boolean): number {
-            let braces = 0,
-                char: number,
-                start: number;
-            while (pos < end) {
-                switch (char = code.charCodeAt(pos)) {
+        const text = ms.original.slice(from, to)
+        let result = text
 
-                    // Handle comments.
-                    case CharCode.slash:
-                        start = pos
-                        char = code.charCodeAt(++pos)
-                        if (char === CharCode.slash) {
+        const matches = Array.from(text.matchAll(re)) as Array<RegExpExecArrayWithGroupsAndIndices<'line' | 'block'>>
+        if (matches.length > 0) {
+            for (let i = matches.length - 1; i >= 0; i--) {
+                const { indices, groups } = matches[i]
+                if (groups.line) {
+                    let [ start, end ] = indices.groups.line!
 
-                            // Go back to the first non-whitespace character before the comment.
-                            while (start > 0 && whitespace.has(code.charCodeAt(start - 1)))
-                                --start
+                    while (start > 0 && space.has(text.charCodeAt(start - 1)))
+                        --start
 
-                            // Advance to the end of the comment's body.
-                            while (pos < end && !lineTerminators.has(code.charCodeAt(pos)))
-                                ++pos
+                    result = result.slice(0, start) + result.slice(end)
+                }
+                else if (groups.block && shouldRemoveComment(groups.block)) {
+                    let [ start, end ] = indices.groups.block!
 
-                            // Include the line terminator if there is no code before the comment.
-                            if (start === 0 || lineTerminators.has(code.charCodeAt(start - 1))) {
-                                ++pos
-                                if (code.charCodeAt(pos) === CharCode.lineFeed && code.charCodeAt(pos - 1) === CharCode.carriageReturn)
-                                    ++pos
-                            }
+                    let before = start
+                    while (before > 0 && space.has(text.charCodeAt(before - 1)))
+                        --before
 
-                            // Unconditionally remove line comments.
-                            ms.remove(start, pos)
-                        }
-                        else if (char === CharCode.asterisk) {
+                    let after = end
+                    while (after < text.length && space.has(text.charCodeAt(after)))
+                        ++after
 
-                            // Get the comment's body.
-                            while (++pos < end && (code.charCodeAt(pos) !== CharCode.slash || code.charCodeAt(pos - 1) !== CharCode.asterisk));
-                            const comment = code.slice(start, ++pos)
+                    if (lineTerminators.has(text.charCodeAt(after))) {
+                        start = before
+                        end = ++after
+                    }
+                    else if (before === 0 || !lineTerminators.has(text.charCodeAt(before - 1)))
+                        start = before
+                    else
+                        end = after
 
-                            // Remove it?
-                            if (shouldRemove(comment)) {
-
-                                // Find the first non-whitespace character before the comment.
-                                let before = start
-                                while (before > 0 && whitespace.has(code.charCodeAt(before - 1)))
-                                    --before
-
-                                // If the comment ends a line and there is no code before the comment, include the line terminator.
-                                // Otherwise, only include all whitespace before the comment, unless it's indentation.
-                                if (lineTerminators.has(code.charCodeAt(pos))) {
-                                    if (before > 0 && lineTerminators.has(code.charCodeAt(before - 1))) {
-                                        ++pos
-                                        if (code.charCodeAt(pos) === CharCode.lineFeed && code.charCodeAt(pos - 1) === CharCode.carriageReturn)
-                                            ++pos
-                                    }
-
-                                    // Also include the whitespace before the comment.
-                                    start = before
-                                }
-                                else if (before === 0 || !lineTerminators.has(code.charCodeAt(before - 1)))
-                                    start = before
-
-                                ms.remove(start, pos)
-                            }
-                        }
-                        break
-
-                    // Skip strings altogether so we don't process their content inadvertently.
-                    case CharCode.quotationMark:
-                    case CharCode.apostrophe:
-                        while (++pos < end) {
-                            if (code.charCodeAt(pos) === CharCode.backslash) {
-                                ++pos
-                                continue
-                            }
-                            if (code.charCodeAt(pos) === char) {
-                                ++pos
-                                break
-                            }
-                        }
-                        break
-
-                    // Entering a template literal.
-                    // If substitution slices (anything between '${' and '}') are found,
-                    // we recursively call ourselves for easy handling of their contents.
-                    case CharCode.backtick:
-                        while (++pos < end) {
-                            if (code.charCodeAt(pos) === CharCode.backslash) {
-                                ++pos
-                                continue
-                            }
-                            if (code.charCodeAt(pos) === CharCode.backtick) {
-                                ++pos
-                                break
-                            }
-                            if (code.charCodeAt(pos) === CharCode.dollar && code.charCodeAt(pos + 1) === CharCode.openBrace) {
-                                pos = scan(pos + 1, true) - 1
-                            }
-                        }
-                        break
-
-                    // When scanning inside a template literal substitution slice, we track brace opening/closing
-                    // so as not to mistake a closing brace for the end of that slice.
-                    case CharCode.openBrace:
-                        ++pos
-                        ++braces
-                        break
-
-                    case CharCode.closeBrace:
-                        ++pos
-                        if (--braces === 0 && inTemplate)
-                            return pos
-                        break
-
-                    default:
-                        ++pos
-                        if (lineTerminators.has(char)) {
-                            if (pos < end && char === CharCode.carriageReturn && code.charCodeAt(pos) === CharCode.lineFeed)
-                                ++pos
-
-                            // Remove empty lines.
-                            start = pos
-                            while (pos < end && lineTerminators.has(code.charCodeAt(pos)))
-                                ++pos
-                            if (pos > start)
-                                ms.remove(start, pos)
-                        }
-                        break
+                    result = result.slice(0, start) + result.slice(end)
                 }
             }
-            return pos
         }
+
+        result = result.replaceAll(/\n+/g, '\n')
+        if (result === '\n')
+            result = ''
+        if (result !== text)
+            ms.update(from, to, result)
     }
 }
