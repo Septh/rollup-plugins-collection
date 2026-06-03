e95782e0c8a67084abdc6421e1d911b36118a589
Stephan Schreiber <septh@users.noreply.github.com>
2026-06-03 12:28:17 +0200

    docs: be more descriptive in the main JsDoc

--- source/index.ts
+++ source/index.ts
@@ -5,14 +5,20 @@ import production, { type ProductionOptions } from './production.js'
 
 export interface CollectionOptions {
     comments?: NoCommentOptions | undefined
-    debug?: ProductionOptions | undefined
+    production?: ProductionOptions | undefined
 }
 
 /**
- * All plugins in the collection with only one import!
+ * All plugins in the collection with only one import. Adds:
+ * - `enums()`
+ * - `noComment(options.comment)`
+ * - `production(options.production)`
+ *
+ * You may also only choose the plugins you need by importing them by their name
+ * rather than the default export.
  */
 export default function collection(options: CollectionOptions = {}): Plugin[] {
-    return [ enums(), noComment(options.comments), production(options.debug) ]
+    return [ enums(), noComment(options.comments), production(options.production) ]
 }
 
 export {
